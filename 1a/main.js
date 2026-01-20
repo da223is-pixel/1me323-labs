2e70baafd5fe5f776258bb2fc048b34e3e8de5e9
Diana Ahmadi <da223is@student.lnu.se>
2026-01-20 19:30:47 +0100

    Lab 1-a: Initial commit with math functions

---
+++ 1a/main.js
@@ -0,0 +1,23 @@
+// import {add, subtract, multiply} from './math.js';
+// console.log(add(2,4));
+// console.log(subtract(1564, 40));
+// console.log(multiply(66, 2));
+
+// import divide from './math.js';
+// console.log(divide(66, 2));
+
+// import divide, { add, subtract, multiply } from './math.js';
+// console.log(add(5, 3));       // 8
+// console.log(divide(20, 4)); 
+
+// import {PI, SECRET_SAUCE} from './math.js';
+// console.log(SECRET_SAUCE);
+// // console.log(`PI är ungefär ${PI}`);
+
+// import { add as addition, subtract as subtraction } from './math.js';
+// console.log(addition(5, 3));  
+
+import * as MathUtils from './math.js';
+console.log (MathUtils);
+console.log(MathUtils.add(2, 3));
+console.log(MathUtils.multiply(4, 5));
