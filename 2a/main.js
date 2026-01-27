bc8f08f485ae816e1f758db1f4ac4d4d8a46b5e9
Diana Ahmadi <da223is@student.lnu.se>
2026-01-27 13:30:58 +0100

    object data in DOM

--- 2a/main.js
+++ 2a/main.js
@@ -29,4 +29,31 @@ console.log(person.firstName);
 console.log(person["lastName"]);
 console.log("personenes stad är:",person.adress.city);
 
+const movie ={
+name: "PK",
+year: 2018,
+ isAvailable: true,
+director:{
+firstName: "Maha",
+},
+ mainActors: ["Amir khan", "Homan azai"]
+};
+console.log(movie);
+console.log(movie.mainActors[0]);
+
+person.hobby= "gym"; // Lägg till ny egenskap
+person.age= 37;//Ändra en befintlig
+delete person.occupation;// Ta bort en egenskap
+console.log("Har personen occupation?", "occupation" in person);// Kontrollera om en egenskap finns
+
+console.log(person.husband); // undefind för den ej finns i objektet.
+
+const movieElement = document.getElementById ("movie");
+
+movieElement.innerHTML =`
+<h2> ${movie.name} ${movie.year}</h2>
+<p> director:${movie.director.firstName} </p>
+<p>Actors: ${movie.mainActors.join(", ")}</p>
+
+`;
 
