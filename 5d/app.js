d970a7b6ced2ff6195189a17251df6b3c684a922
Diana Ahmadi <da223is@student.lnu.se>
2026-02-22 21:28:19 +0100

    Revert "Add multiply function"
    This reverts commit ac99d75255224c73967d50b31696ecf5e703fe59.

--- 5d/app.js
+++ 5d/app.js
@@ -11,14 +11,3 @@ function farewell(name) {
 
 console.log(farewell("Världen"));
 
-function multiply(a, b) {
-  return a + b; // Obs: detta är fel! Borde vara a * b
-}
-
-console.log(multiply(3, 4)); // Skriver ut 7 istället för 12
-
-function divide(a, b) {
-  return a / b;
-}
-
-console.log(divide(10, 2));
